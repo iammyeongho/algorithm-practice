e9af07bb11015a4ccae17255f33bbd03f44dd016
iammyeongho <99030660+iammyeongho@users.noreply.github.com>
2025-04-27 23:14:02 +0900

    [Bronze V] Title: A+B - 7, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.py
@@ -0,0 +1,5 @@
+T = int(input())  # 테스트케이스 개수 입력
+
+for i in range(1, T + 1):  # 1부터 T까지 반복 (케이스 번호를 위해)
+    A, B = map(int, input().split())  # 각 줄마다 A, B 입력
+    print(f"Case #{i}: {A + B}")  # 케이스 번호와 결과 출력
